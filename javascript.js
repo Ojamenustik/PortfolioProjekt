1afa4d54aa82f36db988c2df5d89bb4500d60d9c
Ojamenustik <baka.yarichin@gmail.com>
2018-05-16 19:13:38 +0200

    started menu + css structure with SCSS

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var nav = document.getElementsByTagName('nav');
+var button = document.getElementsByClassName('mobile-button')[0];
+
+if nav
+button.onclick(e, function{
+               nav.addClass('open');
+               })
